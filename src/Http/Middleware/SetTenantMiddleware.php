c223a484081fe99186e0dbfb9773f147e4992f0e
henryavila <henryavila@users.noreply.github.com>
2023-01-24 11:06:15 +0000

    Fix styling

--- src/Http/Middleware/SetTenantMiddleware.php
+++ src/Http/Middleware/SetTenantMiddleware.php
@@ -9,67 +9,66 @@ use Illuminate\Support\Facades\Auth;
 
 class SetTenantMiddleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $route = $request->route();
+        $routeName = $route ? $route->getName() : null;
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param \Closure $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		$route = $request->route();
-		$routeName = $route ? $route->getName() : null;
+        // Don't run middleware if in any route marked to be ignored
+        if (in_array($routeName, config('nova-multitenancy.routes_to_ignore'))) {
+            return $next($request);
+        }
 
-		// Don't run middleware if in any route marked to be ignored
-		if (in_array($routeName, config('nova-multitenancy.routes_to_ignore'))) {
-			return $next($request);
-		}
+        // If tenant is already defined don't run this middleware
+        if (Tenant::current()) {
+            return $next($request);
+        }
 
-		// If tenant is already defined don't run this middleware
-		if (Tenant::current()) {
-			return $next($request);
-		}
+        // Allow The super Admin to enter without a Tenant Selected
+        $user = Auth::user();
+        //		if ($user?->isSuperAdmin() || $request->getRequestUri() === '/select-tenant') {
+        //			return $next($request);
+        //		}
 
-		// Allow The super Admin to enter without a Tenant Selected
-		$user = Auth::user();
-		//		if ($user?->isSuperAdmin() || $request->getRequestUri() === '/select-tenant') {
-		//			return $next($request);
-		//		}
+        // If the user came from Tenant Selection, let's get data from session and save the tenant
+        $tenantId = session()->get(Tenant::TENANT_SELECTOR_SESSION_ID);
 
-		// If the user came from Tenant Selection, let's get data from session and save the tenant
-		$tenantId = session()->get(Tenant::TENANT_SELECTOR_SESSION_ID);
+        if (! empty($tenantId)) {
+            /** @var Tenant $tenant */
+            $tenant = LaravelNovaMultitenancy::getTenantModel($tenantId);
+            if (empty($tenant)) {
+                abort(404);
+            }
 
-		if (!empty($tenantId)) {
-			/** @var Tenant $tenant */
-			$tenant = LaravelNovaMultitenancy::getTenantModel($tenantId);
-			if (empty($tenant)) {
-				abort(404);
-			}
+            $tenant->makeCurrent();
 
-			$tenant->makeCurrent();
+            return $next($request);
+        }
 
-			return $next($request);
-		}
+        // From this point, we have sure that: we have no tenant selected and we are not in login or select tenant routes
+        // (and not in any other route marked to be ignored)
 
-		// From this point, we have sure that: we have no tenant selected and we are not in login or select tenant routes
-		// (and not in any other route marked to be ignored)
+        // The Tenant must be set just to users that can't manage Tenants
+        $totalTenants = $user->tenants->count();
 
-		// The Tenant must be set just to users that can't manage Tenants
-		$totalTenants = $user->tenants->count();
+        if ($totalTenants === 1) {
+            $user->tenants->first()->makeCurrent();
 
-		if ($totalTenants === 1) {
-			$user->tenants->first()->makeCurrent();
+            return $next($request);
+        }
 
-			return $next($request);
-		}
+        if ($user->isSuperAdmin() || $totalTenants > 1) {
+            return redirect()->route('select-tenant');
+        }
 
-		if ($user->isSuperAdmin() || $totalTenants > 1) {
-			return redirect()->route('select-tenant');
-		}
-
-		Auth::logout();
-		abort(403, __('Você precisa estar vinculado a alguma igreja.'));
-	}
+        Auth::logout();
+        abort(403, __('Você precisa estar vinculado a alguma igreja.'));
+    }
 }
