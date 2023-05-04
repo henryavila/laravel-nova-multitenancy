aab9af451ae02287b0b4cf7f6ed21afaec3d0167
henryavila <henryavila@users.noreply.github.com>
2023-05-04 11:52:48 +0000

    Fix styling

--- src/LaravelNovaMultitenancy.php
+++ src/LaravelNovaMultitenancy.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class LaravelNovaMultitenancy
 {
-	const SKIP_ROUTE = 'skipTenantSelection';
+    const SKIP_ROUTE = 'skipTenantSelection';
 
-	public static function getTenantModel(int $id): ?Model
-	{
-		$tenantModelClass = config('nova-multitenancy.tenant_model');
+    public static function getTenantModel(int $id): ?Model
+    {
+        $tenantModelClass = config('nova-multitenancy.tenant_model');
 
-		return $tenantModelClass::find($id);
-	}
+        return $tenantModelClass::find($id);
+    }
 }
--- src/Traits/ModelWithTenant.php
+++ src/Traits/ModelWithTenant.php
@@ -14,26 +14,26 @@ use HenryAvila\LaravelNovaMultitenancy\Scopes\TenantScope;
  */
 trait ModelWithTenant
 {
-	public function __construct(array $attributes = [])
-	{
-		parent::__construct($attributes);
-		$this->setConnection(config('database.tenant_connection'));
-	}
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setConnection(config('database.tenant_connection'));
+    }
 
-	/**
-	 * The "booted" method of the model.
-	 *
-	 * @return void
-	 */
-	protected static function boot()
-	{
-		parent::boot();
-		static::addGlobalScope(new TenantScope());
-		static::observe(new ModelWithTenantObserver());
-	}
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new TenantScope());
+        static::observe(new ModelWithTenantObserver());
+    }
 
-	public function tenant()
-	{
-		return $this->belongsTo(config('nova-multitenancy.tenant_model'));
-	}
+    public function tenant()
+    {
+        return $this->belongsTo(config('nova-multitenancy.tenant_model'));
+    }
 }
