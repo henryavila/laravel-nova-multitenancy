21b7ba74f20468a885d2ae54be65a731e4714d23
henryavila <henryavila@users.noreply.github.com>
2023-09-01 14:04:55 +0000

    Fix styling

--- src/Models/Tenant.php
+++ src/Models/Tenant.php
@@ -15,17 +15,18 @@ class Tenant extends \Spatie\Multitenancy\Models\Tenant
         'disk_usage_in_bytes' => 'int',
     ];
 
-	protected static function booted(): void
-	{
-		parent::booted();
+    protected static function booted(): void
+    {
+        parent::booted();
 
-		static::updating(function ($tenant) {
-			if ($tenant->disk_usage_in_bytes < 0) {
-				$tenant->disk_usage_in_bytes = null;
-			}
-		});
+        static::updating(function ($tenant) {
+            if ($tenant->disk_usage_in_bytes < 0) {
+                $tenant->disk_usage_in_bytes = null;
+            }
+        });
 
-	}
+    }
+
     /**
      * Is there any Tenant selected
      */
