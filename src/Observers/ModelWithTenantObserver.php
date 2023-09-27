9a7361c23989fb8b5af1857d2b82f962419d9ca8
Henry Ávila <dev@henryavila.com>
2023-09-27 16:27:31 -0300

    Allow to disable automatically set user owner for model

--- src/Observers/ModelWithTenantObserver.php
+++ src/Observers/ModelWithTenantObserver.php
@@ -10,25 +10,29 @@ use Illuminate\Support\Facades\Auth;
 
 class ModelWithTenantObserver
 {
-    public function creating(Model $model)
-    {
-        $modelHasUser = $model->getConnection()->getSchemaBuilder()->hasColumn($model->getTable(), 'user_id');
+	public function creating(Model $model)
+	{
+		$modelHasUser = $model->getConnection()->getSchemaBuilder()->hasColumn($model->getTable(), 'user_id');
 
-        if (empty($model->getAttribute('tenant_id'))) {
-            $model->setAttribute('tenant_id', Tenant::current()->id);
-        }
+		if (empty($model->getAttribute('tenant_id'))) {
+			$model->setAttribute('tenant_id', Tenant::current()->id);
+		}
 
-        if ($modelHasUser && empty($model->getAttribute('user_id'))) {
-            /** @var User $authenticatedUser */
-            $authenticatedUser = Auth::user();
+		$automaticallySetModelOwner = config('nova-multitenancy.automatically_set_owner_user_for_classes') !== false;
+		if ($automaticallySetModelOwner && $modelHasUser && empty($model->getAttribute('user_id'))) {
 
-            if ($authenticatedUser) {
-                $userId = $authenticatedUser->id;
-            } else {
-                throw new Exception('The model "'.get_class($model).'" must be linked to a user. No authenticated user found');
-            }
+			if (!in_array($model::class, config('nova-multitenancy.ignore_set_owner_user_for_classes'))) {
+				/** @var User $authenticatedUser */
+				$authenticatedUser = Auth::user();
 
-            $model->setAttribute('user_id', $userId);
-        }
-    }
+				if ($authenticatedUser) {
+					$userId = $authenticatedUser->id;
+				} else {
+					throw new Exception('The model "' . get_class($model) . '" must be linked to a user. No authenticated user found');
+				}
+
+				$model->setAttribute('user_id', $userId);
+			}
+		}
+	}
 }
